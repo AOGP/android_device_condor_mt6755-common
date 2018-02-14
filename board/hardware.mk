d908ee428b67b194adef9db0ac10b1090a4d8c23
ALGPHELLO <khaledannab@gmail.com>
2018-02-14 11:39:06 +0100

    Add Board Configs

---
+++ board/hardware.mk
@@ -0,0 +1,8 @@
+# CM hardware
+BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/cmhw
+
+# LightHAL
+TARGET_PROVIDES_LIBLIGHT := true
+
+# MTK Hardware
+BOARD_USES_MTK_HARDWARE := true
---
+++ board/telephony.mk
@@ -0,0 +1,3 @@
+# RIL
+BOARD_RIL_CLASS := ../../../device/condor/mt6755-common/ril
+BOARD_CONNECTIVITY_MODULE := conn_soc
